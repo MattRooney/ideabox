7e5d7190f36b9bf0fc408c76f83edaa2b9ff6cb0
Matt Rooney <matthewjrooney@gmail.com>
2016-02-04 14:25:29 -0700

    delete empty coffee files and folders

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :idea
+  def create_idea
+    @idea = Idea.new( { id: 1,
+                        title: "Idea Title",
+                        body: "Idea Body",
+                      } )
+  end
+
+
+  test "Idea is valid" do
+    create_idea
+
+    assert idea.valid?
+  end
+
+  test "Idea has a title, body and quality" do
+    create_idea
+
+    assert_equal "Idea Title", idea.title
+    assert_equal "Idea Body", idea.body
+    assert_equal "swill", idea.quality
+  end
 end
